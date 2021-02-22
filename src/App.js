56ad1e16343f4ba47e50d8c4015a5419c9ea817c
Cathal-Butler <73025090+Cathal-Butler@users.noreply.github.com>
2021-02-22 21:15:08 +0000

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import "./styles.css";
+
+export default function App() {
+  return (
+    <div className="App">
+      <h1>Basic Bill</h1>
+      <h2>by Conor, Sinead, Pavel, Cathal and Di-Yun</h2>
+    </div>
+  );
+}
