09d2e4b96da78e9e313f672b0da18779c00d3427
Cathal-Butler <73025090+Cathal-Butler@users.noreply.github.com>
2021-03-08 14:49:24 +0000

    initial commit

--- src/pages/page1.js
+++ src/pages/page1.js
@@ -1,49 +1,48 @@
 import React, { Component } from "react";
 import { withRouter } from "react-router-dom";
 
-class Inputteddata extends React.Component {
-  // Component gives users a summary of the data they just inputted
-  render() {
-    return (
-      <div>
-        <p>
-          Will show confirmation of the inputted data once the submit button is
-          clicked
-        </p>
-      </div>
-    );
-  }
-}
-
 class Page1 extends Component {
   render() {
     return (
       <div id="page1">
         <h1>Enter your data here: </h1>
-        <div id="expense">
-          <h2>Expense </h2>
-          <form>
-            <label>Input Date of Expense: </label>
-            <input id="expensedate" type="date"></input>
-          </form>
-          <label>Amount: </label>
-          <input id="expenseamount" type="number" placeholder="€" />
-          <button id="submitexpense"> Submit Expense </button>
-        </div>
-
-        <div id="income">
-          <h2>Income</h2>
-          <form>
-            <label>Input Date of Income: </label>
-            <input id="incomedate" type="date"></input>
-          </form>
-          <label>Amount: </label>
-          <input id="incomeamount" type="number" placeholder="€" />
-          <button id="submitincome"> Submit Income </button>
-        </div>
-
-        <Inputteddata />
+        <Income />
+        <Expense />
       </div> //end of page1 div
+    );
+  }
+}
+
+class Expense extends Component {
+  render() {
+    return (
+      <div>
+        <h2>Expense </h2>
+        <form>
+          <label>Input Date of Expense: </label>
+          <input id="expensedate" type="date"></input>
+        </form>
+        <label>Amount: </label>
+        <input id="expenseamount" type="number" placeholder="€" />
+        <button id="submitexpense"> Submit Expense </button>
+      </div>
+    );
+  }
+}
+
+class Income extends Component {
+  render() {
+    return (
+      <div id="income">
+        <h2>Income</h2>
+        <form>
+          <label>Input Date of Income: </label>
+          <input id="incomedate" type="date"></input>
+        </form>
+        <label>Amount: </label>
+        <input id="incomeamount" type="number" placeholder="€" />
+        <button id="submitincome"> Submit Income </button>
+      </div>
     );
   }
 }
