eef2812c347a6576cdeeb0da8b3e24c028dc1e0c
axwable <72824621+axwable@users.noreply.github.com>
2021-03-29 16:30:08 +0100

    Login page form by  Pavel

--- src/pages/home.js
+++ src/pages/home.js
@@ -6,6 +6,12 @@ class Home extends Component {
     return (
       <div id="home">
         <h1>This is our Home Page</h1>
+        <input name="email" type="text" placeholder="Enter your email.." />
+        <input name="password" type="password" placeholder="Password.." />
+        <button type="submit">Sign In</button>
+        <p>
+          <button type="submit">Registration</button>
+        </p>
       </div>
     );
   }
