5ff475380a3a837cdc7170cc143c0d0d00f20071
Cathal Butler <73025090+Cathal-Butler@users.noreply.github.com>
2021-04-07 10:16:39 +0100

    Filter Function Tested Displaying data.js data using map & filter

--- src/pages/page2.js
+++ src/pages/page2.js
@@ -4,28 +4,28 @@ import { userData } from "./data.js";
 
 const data = userData;
 
-class Page2 extends Component {
-  constructor(props) {
-    super(props);
-    this.state = { uArray: data };
-  }
+function selectID(id) {
+  return id.userID < 5;
+}
 
+class Page2 extends Component {
   render() {
+    console.log(data[0]);
+
     return (
-      <div>
-        <h1>Data Object (Map Function Test): </h1>
-        {this.state.uArray.map((data) => {
-          return (
-            <div>
-              <li>
-                <b>UserID: </b> {" " + data.userID + " "}
-                <b> Name: </b> {" " + data.name + " "}
-                <b> Expense: </b> {" " + data.expenses + " "}
-                <b> Income: </b> {" " + data.income + " "}{" "}
-              </li>
-            </div>
-          );
-        })}
+      <div className="App">
+        <h1>Data Object (Map Function/Filter Function Test): </h1>
+        <p>Test Paragraph </p>
+        {data.filter(selectID).map((d) => (
+          <div key={d.userID}>
+            <li>
+              <b>UserID: </b> {" " + d.userID + " "}
+              <b> Name: </b> {" " + d.name + " "}
+              <b> Expense: </b> {" " + d.expenses + " "}
+              <b> Income: </b> {" " + d.income + " "}{" "}
+            </li>
+          </div>
+        ))}
       </div>
     );
   }
