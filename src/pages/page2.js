924a4a489ca7234cd13cc15e09f228c16630e0a4
DIYUN-1998 <57675566+DIYUN-1998@users.noreply.github.com>
2021-04-08 22:53:28 +0100

    financial view (pie and bar chart)

--- src/pages/page2.js
+++ src/pages/page2.js
@@ -1,6 +1,15 @@
 import React, { Component } from "react";
 import { withRouter } from "react-router-dom";
 import { userData } from "./data.js";
+import ReactEcharts from 'echarts-for-react'
+import $ from "jquery";
+//导入折线图
+import 'echarts/lib/chart/line'
+// 引入提示框和标题组件
+import 'echarts/lib/component/tooltip'
+import 'echarts/lib/component/title'
+import 'echarts/lib/component/legend'
+import 'echarts/lib/component/markPoint'
 
 const data = userData;
 function flexibleSelectID(id) {
@@ -9,7 +18,12 @@ function flexibleSelectID(id) {
   };
 }
 
+
 class Page2 extends Component {
+  piechartClick(){
+    $("#piechart").style.display!== 'none' ? 'none' : 'block'
+  }
+
   render() {
     console.log(data);
 
@@ -26,10 +40,136 @@ class Page2 extends Component {
               <b> Income: </b> {" " + d.income + " "}{" "}
             </li>
           </div>
-        ))}
+          ))}
+          <button onClick={this.piechartClick}>  pie chart</button>
+          <button onClick={this.linechartClick}> line chart</button>
+        <div id="piechart" display="none">
+          <ReactEcharts 
+option={{
+  tooltip: {
+      trigger: 'item',
+      formatter: '{a} <br/>{b}: {c} ({d}%)'
+  },
+  legend: {
+      orient: 'vertical',
+      left: 10,
+      data: ['income', 'expense']
+  },
+  series: [
+  {
+      name: 'Utilization',
+      type: 'pie',
+      radius: ['50%', '70%'],
+      avoidLabelOverlap: false,
+      label: {
+          show: false,
+          position: 'center'
+      },
+      emphasis: {
+          label: {
+              show: true,
+              fontSize: '30',
+              fontWeight: 'bold'
+          }
+      },
+      labelLine: {
+          show: false
+      },
+      data: [
+          {value: 335, name: 'income'},
+          {value: 310, name: 'expense'},
+         
+      ]
+    }
+  ]
+}}
+/>       
+    </div>
+<div id="linechart" display="none">
+<ReactEcharts
+
+option = {{
+  tooltip: {
+      trigger: 'axis',
+      axisPointer: {            // 坐标轴指示器，坐标轴触发有效
+          type: 'shadow'        // 默认为直线，可选为：'line' | 'shadow'
+      }
+  },
+  legend: {
+      data: ['balance', 'expense', 'income']
+  },
+  grid: {
+      left: '3%',
+      right: '4%',
+      bottom: '3%',
+      containLabel: true
+  },
+  xAxis: [
+      {
+          type: 'value'
+      }
+  ],
+  yAxis: [
+      {
+          type: 'category',
+          axisTick: {
+              show: false
+          },
+          data: ['mon', 'tues', 'wed', 'thur', 'fri', 'sat', 'sun']
+      }
+  ],
+  series: [
+      {
+          name: 'balance',
+          type: 'bar',
+          label: {
+              show: true,
+              position: 'inside'
+          },
+          emphasis: {
+              focus: 'series'
+          },
+          data: [200, 170, 240, 244, 200, 220, 210]
+      },
+      {
+          name: 'income',
+          type: 'bar',
+          stack: 'total',
+          label: {
+              show: true
+          },
+          emphasis: {
+              focus: 'series'
+          },
+          data: [320, 302, 341, 374, 390, 450, 420]
+      },
+      {
+          name: 'expense',
+          type: 'bar',
+          stack: 'total',
+          label: {
+              show: true,
+              position: 'left'
+          },
+          emphasis: {
+              focus: 'series'
+          },
+          data: [-120, -132, -101, -134, -190, -230, -210]
+      }
+]
+}
+}
+/>
+
+</div>
       </div>
+
+
+
     );
   }
 }
 
 export default withRouter(Page2);
+
+
