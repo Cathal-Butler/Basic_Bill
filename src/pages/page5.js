bf6ef6e0b4db032ae77cdc980671ce47e86f3036
Cathal Butler <73025090+Cathal-Butler@users.noreply.github.com>
2021-05-10 09:44:35 +0100

    Commented pg5

--- src/pages/page5.js
+++ src/pages/page5.js
@@ -1,199 +1,204 @@
-import React, { Component } from "react";
-import { withRouter } from "react-router-dom";
-import myFirebase from ".././myFirebaseConfig";
-import Firebase from "firebase";
+//THIS IS A DEMO PAGE WHICH TESTS THE FIREBASE CRUD FUNCTIONALITY
+//A USER IS ABLE OT CREATE AN OBJECT,
+//VIEW THEIR OBJECT
+//DELETE THEIR OBJECT
 
-class Page5 extends Component {
-  constructor(props) {
-    super(props);
-    //In essence, we will create a copy of the Firebase database and store it in state dbData[]
-    this.state = {
-      dbData: []
-    };
+// import React, { Component } from "react";
+// import { withRouter } from "react-router-dom";
+// import myFirebase from ".././myFirebaseConfig";
+// import Firebase from "firebase";
 
-    //Inform react of events
-    this.getMessagesFromDatabase = this.getMessagesFromDatabase.bind(this);
-    this.generateRandomObject = this.generateRandomObject.bind(this);
-    this.addExpense = this.addExpense.bind(this);
-  } // end constructor
+// class Page5 extends Component {
+//   constructor(props) {
+//     super(props);
+//     //In essence, we will create a copy of the Firebase database and store it in state dbData[]
+//     this.state = {
+//       dbData: []
+//     };
 
-  componentDidMount() {
-    // as soon as the component mounts, get the most recent messages from the firebase database.
+//     //Inform react of events
+//     this.getMessagesFromDatabase = this.getMessagesFromDatabase.bind(this);
+//     this.generateRandomObject = this.generateRandomObject.bind(this);
+//     this.addExpense = this.addExpense.bind(this);
+//   } // end constructor
 
-    this.getMessagesFromDatabase();
-  }
-  //Alot of the code below is from firebase which handles the data
-  getMessagesFromDatabase() {
-    let user = Firebase.auth().currentUser; //For users to access their own specific data
-    let uid = user.uid; //Grabs the automatically generated firebase authenticated user ID
-    let ref = Firebase.database().ref("userData").child(uid); //CHANGE**.child(uid) is added so users access their own data only
+//   componentDidMount() {
+//     // as soon as the component mounts, get the most recent messages from the firebase database.
 
-    ref.on("value", (snapshot) => {
-      // json array
-      let msgData = snapshot.val();
-      let newMessagesFromDB = [];
-      for (let m in msgData) {
-        // create a JSON object version of our object which is blank for now. If the JSON data format changes in the database, this code will need to change
-        let currObject = {
-          objectID: msgData[m].objectID,
-          details: {
-            expenses: msgData[m].details.expenses,
-            expensesDate: msgData[m].details.expensesDate,
-            income: msgData[m].details.income,
-            incomeDate: msgData[m].details.incomeDate,
-            invoiceAmount: msgData[m].details.invoiceAmount,
-            invoiceDue: msgData[m].details.invoiceDue
-          }
-        };
-        // add it to our newStateMessages array.
-        newMessagesFromDB.push(currObject);
-      } // end for loop
-      // set state
-      this.setState({ dbData: newMessagesFromDB });
-    }); // end of the on method
-  } // end of getMessagesFromDatabase()
+//     this.getMessagesFromDatabase();
+//   }
+//   //Alot of the code below is from firebase which handles the data
+//   getMessagesFromDatabase() {
+//     let user = Firebase.auth().currentUser; //For users to access their own specific data
+//     let uid = user.uid; //Grabs the automatically generated firebase authenticated user ID
+//     let ref = Firebase.database().ref("userData").child(uid); //CHANGE**.child(uid) is added so users access their own data only
 
-  //CREATE ACTIVITY
-  generateRandomObject() {
-    let uuid = require("uuid"); //generate unique message ID
-    let objectID = uuid.v4(); //auto generated objectID from Firebase
-    let expensesLabel = [""];
-    let expensesDateLabel = [""];
-    let incomeLabel = [""];
-    let incomeDateLabel = [""];
-    let invoiceAmountLabel = [""];
-    let invoiceDueLabel = [""];
-    //create new message object which will be inserted into the DB.
-    let newMsgObj = {
-      objectID: objectID,
-      details: {
-        expenses: expensesLabel,
-        expensesDate: expensesDateLabel,
-        income: incomeLabel,
-        incomeDate: incomeDateLabel,
-        invoiceAmount: invoiceAmountLabel,
-        invoiceDue: invoiceDueLabel
-      }
-    };
+//     ref.on("value", (snapshot) => {
+//       // json array
+//       let msgData = snapshot.val();
+//       let newMessagesFromDB = [];
+//       for (let m in msgData) {
+//         // create a JSON object version of our object which is blank for now. If the JSON data format changes in the database, this code will need to change
+//         let currObject = {
+//           objectID: msgData[m].objectID,
+//           details: {
+//             expenses: msgData[m].details.expenses,
+//             expensesDate: msgData[m].details.expensesDate,
+//             income: msgData[m].details.income,
+//             incomeDate: msgData[m].details.incomeDate,
+//             invoiceAmount: msgData[m].details.invoiceAmount,
+//             invoiceDue: msgData[m].details.invoiceDue
+//           }
+//         };
+//         // add it to our newStateMessages array.
+//         newMessagesFromDB.push(currObject);
+//       } // end for loop
+//       // set state
+//       this.setState({ dbData: newMessagesFromDB });
+//     }); // end of the on method
+//   } // end of getMessagesFromDatabase()
 
-    let user = Firebase.auth().currentUser; //CHANGE*** Added to access user's data only
-    let uid = user.uid; //CHANGE*** Access users firebase ID
-    //access the messages held in state held above
-    let localMessages = this.state.dbData;
-    //add our new object, can use push here.
-    localMessages.push(newMsgObj);
-    //For basic write ops, can use set() to save data to specified ref, replacing any existing data at that path.
-    //Using set() overwrites data at that specified location incl. any child data
-    Firebase.database().ref("/userData").child(uid).set(localMessages); //CHANGE**** Added to access user's data only
-    //restore state of this component back to the default
-    //The entire array of JSON objects(incl. the new object) are pushed and state is set again
-    this.setState({ dbData: localMessages });
-  }
+//   //CREATE ACTIVITY
+//   generateRandomObject() {
+//     let uuid = require("uuid"); //generate unique message ID
+//     let objectID = uuid.v4(); //auto generated objectID from Firebase
+//     let expensesLabel = [""];
+//     let expensesDateLabel = [""];
+//     let incomeLabel = [""];
+//     let incomeDateLabel = [""];
+//     let invoiceAmountLabel = [""];
+//     let invoiceDueLabel = [""];
+//     //create new message object which will be inserted into the DB.
+//     let newMsgObj = {
+//       objectID: objectID,
+//       details: {
+//         expenses: expensesLabel,
+//         expensesDate: expensesDateLabel,
+//         income: incomeLabel,
+//         incomeDate: incomeDateLabel,
+//         invoiceAmount: invoiceAmountLabel,
+//         invoiceDue: invoiceDueLabel
+//       }
+//     };
 
-  addExpense() {
-    let newExpense = 5000;
+//     let user = Firebase.auth().currentUser; //CHANGE*** Added to access user's data only
+//     let uid = user.uid; //CHANGE*** Access users firebase ID
+//     //access the messages held in state held above
+//     let localMessages = this.state.dbData;
+//     //add our new object, can use push here.
+//     localMessages.push(newMsgObj);
+//     //For basic write ops, can use set() to save data to specified ref, replacing any existing data at that path.
+//     //Using set() overwrites data at that specified location incl. any child data
+//     Firebase.database().ref("/userData").child(uid).set(localMessages); //CHANGE**** Added to access user's data only
+//     //restore state of this component back to the default
+//     //The entire array of JSON objects(incl. the new object) are pushed and state is set again
+//     this.setState({ dbData: localMessages });
+//   }
 
-    let user = Firebase.auth().currentUser;
-    let uid = user.uid;
+//   addExpense() {
+//     let newExpense = 5000;
 
-    let localMessages = this.state.dbData;
+//     let user = Firebase.auth().currentUser;
+//     let uid = user.uid;
 
-    console.log(localMessages);
-    localMessages[0].details.expenses.push(newExpense);
-    Firebase.database().ref("/userData").child(uid).set(localMessages);
+//     let localMessages = this.state.dbData;
 
-    this.setState({ dbData: localMessages });
-  }
-  //DELETE ACTIVITY
+//     console.log(localMessages);
+//     localMessages[0].details.expenses.push(newExpense);
+//     Firebase.database().ref("/userData").child(uid).set(localMessages);
 
-  //Filter callback function, want to filter out object required for deltion.
-  //Want to match objects which don't match the userObjectID. The filter will return the
-  //array of objects WITHOUT the object with the userObjectID
-  filterByObjectID(userObjectID) {
-    return function (userObject) {
-      return userObject.objectID !== userObjectID;
-    };
-  } //end of filterByUserID
+//     this.setState({ dbData: localMessages });
+//   }
+//   //DELETE ACTIVITY
 
-  //delete the user object form the user selection
-  deleteObject(userObjectIDToDelete) {
-    //get current state in array dbData holding our data
-    const localUserObjects = this.state.dbData;
+//   //Filter callback function, want to filter out object required for deltion.
+//   //Want to match objects which don't match the userObjectID. The filter will return the
+//   //array of objects WITHOUT the object with the userObjectID
+//   filterByObjectID(userObjectID) {
+//     return function (userObject) {
+//       return userObject.objectID !== userObjectID;
+//     };
+//   } //end of filterByUserID
 
-    //filter function needs to be applied to remove object we wish to delete
-    //This is our dbData array WITHOUT the object for deletion.
+//   //delete the user object form the user selection
+//   deleteObject(userObjectIDToDelete) {
+//     //get current state in array dbData holding our data
+//     const localUserObjects = this.state.dbData;
 
-    const updatedlocalUserObjects = localUserObjects.filter(
-      this.filterByObjectID(userObjectIDToDelete)
-    );
+//     //filter function needs to be applied to remove object we wish to delete
+//     //This is our dbData array WITHOUT the object for deletion.
 
-    //set state in the application. Now the local state is updated
-    this.setState({ dbData: updatedlocalUserObjects });
+//     const updatedlocalUserObjects = localUserObjects.filter(
+//       this.filterByObjectID(userObjectIDToDelete)
+//     );
 
-    //updated FB DB using set from Firebase API
-    //replace data at userData with the new dbData array (in state)
-    //or using the local variable
-    let user = Firebase.auth().currentUser; //CHANGE*** Added to get current users data
-    let uid = user.uid; //CHANGE*** Added to get current users ID
-    Firebase.database()
-      .ref("/userData")
-      .child(uid)
-      .set(updatedlocalUserObjects); //CHANGE*** .child() added to access the users data only
-  }
+//     //set state in the application. Now the local state is updated
+//     this.setState({ dbData: updatedlocalUserObjects });
 
-  render() {
-    return (
-      <div className="Logout">
-        <h1>Private Content DB</h1>
+//     //updated FB DB using set from Firebase API
+//     //replace data at userData with the new dbData array (in state)
+//     //or using the local variable
+//     let user = Firebase.auth().currentUser; //CHANGE*** Added to get current users data
+//     let uid = user.uid; //CHANGE*** Added to get current users ID
+//     Firebase.database()
+//       .ref("/userData")
+//       .child(uid)
+//       .set(updatedlocalUserObjects); //CHANGE*** .child() added to access the users data only
+//   }
 
-        {this.state.dbData.length <= 0 && <p>Please wait ... data loading</p>}
+//   render() {
+//     return (
+//       <div className="Logout">
+//         <h1>Private Content DB</h1>
 
-        {this.state.dbData.length > 0 && ( //Should update live if the database changes
-          <p>There are {this.state.dbData.length} objects from DB</p>
-        )}
+//         {this.state.dbData.length <= 0 && <p>Please wait ... data loading</p>}
 
-        <ul>
-          {this.state.dbData.map((
-            data,
-            index //testing the map function on the data
-          ) => (
-            <li key={index}>
-              <b> ObjectID: {data.objectID} </b>
-              <b> Expenses: {"€" + data.details.expenses + ","} </b>{" "}
-              <b> Expense Date: {data.details.expensesDate + ","} </b>{" "}
-              <b> Income: {"€" + data.details.income + ","} </b>
-              <b> Income Date: {data.details.incomeDate + ","} </b>
-              <b>
-                {" "}
-                Invoice Amount: {"€" + data.details.invoiceAmount + ","}{" "}
-              </b>{" "}
-              <b> Invoice Due: {data.details.invoiceDue + ","} </b>
-            </li>
-          ))}
-        </ul>
-        <button onClick={this.generateRandomObject}>Generate Object</button>
+//         {this.state.dbData.length > 0 && ( //Should update live if the database changes
+//           <p>There are {this.state.dbData.length} objects from DB</p>
+//         )}
 
-        <ul>
-          {this.state.dbData.map((data, index) => (
-            <li key={index}>
-              <b> Object ID: {data.objectID} </b>{" "}
-              <b> Expenses: {"€" + data.details.expenses + ","} </b>{" "}
-              <b> Expense Date: {data.details.expensesDate + ","} </b>{" "}
-              <b> Income: {"€" + data.details.income + ","} </b>
-              <b> Income Date: {data.details.incomeDate + ","} </b>
-              <b> Invoice Amount: {data.details.invoiceAmount + ""} </b>{" "}
-              <b> Invoice Due: {data.details.invoiceDue + ","} </b>
-              <button onClick={() => this.deleteObject(data.objectID)}>
-                Delete Object
-              </button>
-            </li>
-          ))}
-        </ul>
+//         <ul>
+//           {this.state.dbData.map((
+//             data,
+//             index //testing the map function on the data
+//           ) => (
+//             <li key={index}>
+//               <b> ObjectID: {data.objectID} </b>
+//               <b> Expenses: {"€" + data.details.expenses + ","} </b>{" "}
+//               <b> Expense Date: {data.details.expensesDate + ","} </b>{" "}
+//               <b> Income: {"€" + data.details.income + ","} </b>
+//               <b> Income Date: {data.details.incomeDate + ","} </b>
+//               <b>
+//                 {" "}
+//                 Invoice Amount: {"€" + data.details.invoiceAmount + ","}{" "}
+//               </b>{" "}
+//               <b> Invoice Due: {data.details.invoiceDue + ","} </b>
+//             </li>
+//           ))}
+//         </ul>
+//         <button onClick={this.generateRandomObject}>Generate Object</button>
 
-        <button onClick={this.addExpense}>Add new Expense </button>
-      </div>
-    ); // end of return statement
-  } // end of render function
-} // end of class
+//         <ul>
+//           {this.state.dbData.map((data, index) => (
+//             <li key={index}>
+//               <b> Object ID: {data.objectID} </b>{" "}
+//               <b> Expenses: {"€" + data.details.expenses + ","} </b>{" "}
+//               <b> Expense Date: {data.details.expensesDate + ","} </b>{" "}
+//               <b> Income: {"€" + data.details.income + ","} </b>
+//               <b> Income Date: {data.details.incomeDate + ","} </b>
+//               <b> Invoice Amount: {data.details.invoiceAmount + ""} </b>{" "}
+//               <b> Invoice Due: {data.details.invoiceDue + ","} </b>
+//               <button onClick={() => this.deleteObject(data.objectID)}>
+//                 Delete Object
+//               </button>
+//             </li>
+//           ))}
+//         </ul>
 
-export default withRouter(Page5);
+//         <button onClick={this.addExpense}>Add new Expense </button>
+//       </div>
+//     ); // end of return statement
+//   } // end of render function
+// } // end of class
+
+// export default withRouter(Page5);
